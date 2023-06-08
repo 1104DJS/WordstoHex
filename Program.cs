c657537018fbf77396a7a9ee7421319b362ac3e7
1104DJS <danieljswery@gmail.com>
2023-06-08 12:36:41 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/* The Program:
+ * 
+ * The program takes the first three characters of the word and gets their corresponding ASCII Hex Code anything < 3 letters will get 00 to fill the end
+ * "Hello, my name is Gary and I like cheese." would return an array of: ['#48656c', '#6d7900', '#6e616d','#697300','#476172','#616e64','#490000','#6c696b','#636865']
+ */
+
+
+namespace W2H
+{
+	class MainProgram
+	{
+		static void Main()
+		{
+			
+			
+		}
+
+		static string[] Word2Hex(string s)
+		{
+		
+		}
+	}
+}
