599886b411a4417fb7d9a469268024517d817975
dsjw04 <danieljswery@gmail.com>
2023-07-16 17:15:08 +0100

    6kyu/expandedForm1

---
+++ 6kyu/expandedForm1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine(ExpandedForm(70304));
+
+static string ExpandedForm(long num) {
+    List<char> charList = num.ToString().ToList<char>();   
+    int totalChar = num.ToString().Length;
+    int charPos = 1; int charNum; int count = 0;
+    int secIndex = 0;
+
+
+    foreach (char s in charList) { if (s != '0') count++;}
+    string[] stringArr = new string[count];
+
+    for (int i = 0; i < totalChar; i++, charPos++) {
+        charNum = totalChar - charPos;
+        if (charList[i] != '0') {
+            stringArr[secIndex] = charList[i].ToString();
+            for (int j = 0; j < charNum; j++) { stringArr[secIndex] += "0"; }
+            secIndex++;
+        }
+    }
+
+    return string.Join(" + ", stringArr);
+}
