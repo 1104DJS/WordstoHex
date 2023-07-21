279b17f8c610e1dbd2b7a6474263da3001fb171d
dsjw04 <danieljswery@gmail.com>
2023-07-21 15:40:12 +0100

    evenFibonacciSum

---
+++ 6kyu/evenFibonacciSum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+System.Console.WriteLine(Fibonacci(0));
+System.Console.WriteLine(Fibonacci(33));
+System.Console.WriteLine(Fibonacci(25997544));
+
+static long Fibonacci(int max) {
+    int sum = 0; int prev = 0; int current = 1;
+
+    while (current <= max) {
+        sum += current * (current % 2 == 0 ? 1 : 0);
+        current = prev + (prev = current);
+    }
+    return sum;
+}
