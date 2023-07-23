00bae40967cce49f8580b8e76822d5b3f2b2e239
dswery <danieljswery@gmail.com>
2023-07-23 21:41:30 +0100

    BinomialExpansion Completed

--- 3kyu/BinomialExpansion/Program.cs
+++ 3kyu/BinomialExpansion/Program.cs
@@ -1,10 +1,20 @@
 ﻿using System.Text;
+using System.Numerics;
 
-Expand("(-x-2)^2");
+//Console.WriteLine(Expand("(-x-2)^2"));
+//Console.WriteLine(Expand("(x-2)^2"));
+//Console.WriteLine(Expand("(p-1)^3"));
+//System.Console.WriteLine(Expand("(-5m+3)^4"));
+//System.Console.WriteLine(Expand("(-7x-7)^0"));
+//System.Console.WriteLine(Expand("(-11x+0)^2")); // 121x^2
+//System.Console.WriteLine(Expand("(-q+19)^3"));
+//System.Console.WriteLine(Expand("(-3l+0)^5")); //-243l^5
+System.Console.WriteLine(Expand("(-4s+15)^8"));
 // (A+B)^C
 
-static void Expand(string expr){
+static string Expand(string expr){
     List<char> exprList = expr.ToList();
+    List<BigInteger> coefficientList = new();
     int locationOfMainOperator;
     int locationOfClosedBracket = expr.IndexOf(')');
     int locationOfPower = expr.IndexOf('^');
@@ -12,7 +22,7 @@ static void Expand(string expr){
     string valueOfA = string.Empty;
     string valueOfB = string.Empty;
     string valueOfC = string.Empty;
-    char signOfA;
+    char signOfA = ' ';
     bool? isAnegative = null;
     bool propertyOfMainOperator; // (T = +)(F = -)
 
@@ -38,34 +48,75 @@ static void Expand(string expr){
     }
 
     // gets value of power 
-    for (int i = locationOfPower; i < expr.Length - 1; i++){
-        if (char.IsNumber(exprList[i])) { valueOfC += exprList[i];}
+    for (int i = locationOfPower; i <= expr.Length - 1; i++){
+        if (char.IsNumber(expr[i])) { valueOfC += expr[i];}
     }
 
+    if (valueOfC == "0") { return "1";}
+
     // expand binomial expression
-    StringBuilder expandedExpression = new();
-    int a = 0, b, c;
+    int a, b, c;
     if (isAnegative == true) { a = -(int.Parse(valueOfA!)); }
+    else { a = int.Parse(valueOfA!); }
     if (!propertyOfMainOperator) { b = -(int.Parse(valueOfB)); }
     else { b = int.Parse(valueOfB); }
     c = int.Parse(valueOfC);
 
     for (int i = 0; i <= c; i++)
     {
-        //insert magic here that expands and outputs in correct form somehow please
-        expandedExpression.Append(BinomialCoefficient(c, i) * Math.Pow(a, c - i) * Math.Pow(b, i));
+        coefficientList.Add((BigInteger)(BinomialCoefficient(c, i) * (BigInteger)Math.Pow(a, c - i) * (BigInteger)Math.Pow(b, i)));
     }
 
-    Console.WriteLine(expandedExpression.ToString());
+    return finalExpression(coefficientList, c, signOfA);
 }
 
 // NCR
-static double BinomialCoefficient(int n, int k) {
-    double result = 1;
+static BigInteger BinomialCoefficient(int n, int k)
+{
+    BigInteger result = BigInteger.One;
 
-    for (int i = 1; i <= k; i++) {
-        result *= (n - i + 1) / (double) i;
+    for (int i = 1; i <= k; i++)
+    {
+        result = result * (n - i + 1) / i;
     }
 
     return result;
+}
+
+static string finalExpression(List<BigInteger> ints, int power, char symbol) {
+    StringBuilder expr = new();
+    for (int i = 0; i <= power; i++){
+        if (i < power - 1) {
+            if (ints[i] > 0) {
+                    if (ints[i] == 1) { expr.Append(symbol).Append('^').Append(power - i); }
+                    else if (ints[i] == 0) { continue; }
+                    else { expr.Append(ints[i]).Append(symbol).Append('^').Append(power - i); }
+                    if (ints[i+1] > 0) { expr.Append('+');}
+            }
+            else {
+                    if (ints[i] == 1) { expr.Append(symbol).Append('^').Append(power - i); }
+                    else if (ints[i] == -1) { expr.Append('-').Append(symbol).Append('^').Append(power - i);}
+                    else if (ints[i] == 0) { continue; }
+                    else { expr.Append(ints[i]).Append(symbol).Append('^').Append(power - i);}
+                    if (ints[i+1] > 0) { expr.Append('+');}
+            }
+        }
+        else if (i == power - 1) {
+            if (ints[i] > 0) {
+                    if (ints[i] == 1) { expr.Append(symbol); }
+                    else if (ints[i] == 0) { continue; }
+                    else { expr.Append(ints[i]).Append(symbol); }
+            }
+            else {
+                    if (ints[i] == 1) { expr.Append(symbol); }
+                    else if (ints[i] == 0) { continue; }
+                    else { expr.Append(ints[i]).Append(symbol);}
+            }
+        }
+        else if (i == power) {
+            if (ints[i] > 0) { expr.Append('+'); expr.Append(ints[i]);}
+            else if (ints[i] < 0) { expr.Append(ints[i]); }
+        }
+    }
+    return expr.ToString();
 }
