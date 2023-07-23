8405c549c0902496437a016175c9fd79791be17c
dswery <danieljswery@gmail.com>
2023-07-23 21:42:44 +0100

    CalculateBMI

---
+++ 8kyu/calculateBMI/Program.cs
@@ -0,0 +1,9 @@
+﻿static string Bmi(double weight, double height) {
+    double bmi = weight / (height * height);
+    if (bmi <= 18.5) return "Underweight";
+    if (bmi <= 25.0) return "Normal";
+    if (bmi <= 30.0) return "Overweight";
+    return "Obese";
+}
+
+Console.WriteLine(Bmi(80, 1.80));
